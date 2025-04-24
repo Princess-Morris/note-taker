b75a0ea42a8eb6844189616ac4e63b862013bbaf
Princess-Morris <okpataprincessmorris3@gmail.com>
2025-04-24 13:56:41 +0100

    style: styling modal feature

--- app/notes/index.jsx
+++ app/notes/index.jsx
@@ -54,6 +54,21 @@ const NoteScreen = () => {
                     value={newNote}
                     onChangeText={setNewNote}
                     >
+                    <View style={styles.modalButtons}>
+                        <TouchableOpacity style={styles.cancelButton}
+                        onPress={() => setModalVisible(false)}
+                        >
+                          <Text style={styles.cancelButtonText}>Cancel</Text>
+                        </TouchableOpacity>
+
+                        {/* <TouchableOpacity style={styles.saveButton}
+                        onPress={addNote}
+                        >
+                          <Text style={styles.saveButtonText}>Save</Text>
+                        </TouchableOpacity> */}
+
+                       
+                        </View>    
 
                     </TextInput>
                 </View>
@@ -94,6 +109,59 @@ const styles = StyleSheet.create({
         color: "#fff",
         fontSize: 16,
         fontWeight: "bold"
+    },
+    modalOverlay: {
+        flex: 1,
+        backgroundColor: "rgba(0,0,0,0.5)",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    modalContent: {
+            backgroundColor: "#fff",
+            padding: 20,
+            borderRadius: 10,
+            width: "80%",
+    },
+    modalTitle: {
+           fontSize: 20,
+           fontWeight: "bold",
+           marginBottom: 10,
+           textAlign: "center",
+    },
+    input: {
+            borderWidth: 1,
+            borderColor: "#ccc",
+            borderRadius: 8,
+            padding: 10,
+            fontSize: 16,
+            marginBottom: 15
+    },
+    modalButtons: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+    cancelButton: {
+        backgroundColor: "#ccc,",
+        padding: 10,
+        borderRadius: 5,
+        flex: 1,
+        marginRight: 5,
+        alignItems: "center",
+    },
+    cancelButtonText: {
+        fontSize: 16,
+        color: "#333"
+    },
+    saveButton: {
+        backgroundColor: "#007bff",
+        padding: 10,
+        borderRadius: 5,
+        flex: 1,
+        alignItems: "center",
+    },
+    saveButtonText: {
+        fontSize: 16,
+        color: "#fff"
     }
 })
 
