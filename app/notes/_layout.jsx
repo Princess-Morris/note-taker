78b584d06df69025300be75726b218c6d86e8738
Princess-Morris <okpataprincessmorris3@gmail.com>
2025-04-24 13:01:40 +0100

    update on stack structuring from Note-page

--- app/notes/_layout.jsx
+++ app/notes/_layout.jsx
@@ -1,7 +1,11 @@
 import { Stack } from "expo-router";
 
 const NoteLayout = () => {
-    return <Stack />
+    return <Stack
+    screenOptions={{
+        headerShown: false,
+    }}
+     />
 }
 
 export default NoteLayout;
